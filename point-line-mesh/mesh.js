87feac58f378c13425655a0425ab644976438b13
imsunhao <imsunhao@vip.163.com>
2025-04-09 12:08:55 +0800

    feat: 渲染圆桶形

--- point-line-mesh/mesh.js
+++ point-line-mesh/mesh.js
@@ -1,27 +1,11 @@
 import * as THREE from 'three';
 
-const geometry = new THREE.BufferGeometry();
+const geometry = new THREE.CylinderGeometry(50, 50, 80);
 
-const vertices = new Float32Array([
-  0, 0, 0,
-  100, 0, 0,
-  0, 100, 0,
-  100, 100, 0
-]);
-
-const attribute = new THREE.BufferAttribute(vertices, 3);
-geometry.attributes.position = attribute;
-
-const indexes = new Uint16Array([
-  0, 1, 2, 2, 1, 3
-  // 0, 1, 2, 2, 3, 1 // 从相机看过去的方向，如果一个三角形是逆时针连接的顶点，就是正面，顺时针就是反面。
-]);
-geometry.index = new THREE.BufferAttribute(indexes, 1);
-
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshBasicMaterial(({
   color: new THREE.Color('red'),
-  side: THREE.DoubleSide, // 双面渲染
-});
+  wireframe: true,
+}));
 
 const mesh = new THREE.Mesh(geometry, material);
 
