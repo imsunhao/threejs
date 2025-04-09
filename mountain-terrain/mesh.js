bed5c978d09a50de186c46fc001a2599a5563700
imsunhao <imsunhao@vip.163.com>
2025-04-09 12:32:34 +0800

    起伏的山脉地形的效果

---
+++ mountain-terrain/mesh.js
@@ -0,0 +1,37 @@
+import * as THREE from 'three';
+import { createNoise2D } from "simplex-noise";
+
+const geometry = new THREE.PlaneGeometry(3000, 3000, 100, 100);
+
+const noise2D = createNoise2D();
+
+updatePosition()
+
+const material = new THREE.MeshBasicMaterial({
+  color: new THREE.Color('orange'),
+  wireframe: true
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+mesh.rotateX(- Math.PI / 2);
+
+console.log(mesh);
+
+export default mesh;
+
+
+export function updatePosition() {
+  const positions = geometry.attributes.position;
+
+  for (let i = 0; i < positions.count; i++) {
+    const x = positions.getX(i);
+    const y = positions.getY(i);
+
+    const z = noise2D(x / 300, y / 300) * 50;
+    const sinNum = Math.sin(Date.now() * 0.002 + x * 0.05) * 10;
+
+    positions.setZ(i, z + sinNum);
+  }
+  positions.needsUpdate = true;
+}
+
