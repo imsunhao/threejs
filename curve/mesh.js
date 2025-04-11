3f6a6c4130dd6f100107744c7b1817f9044f373a
imsunhao <imsunhao@vip.163.com>
2025-04-11 12:30:30 +0800

    feat: 绘制4/1的椭圆

--- curve/mesh.js
+++ curve/mesh.js
@@ -1,18 +1,17 @@
 import * as THREE from 'three';
 
-const arc = new THREE.EllipseCurve(0, 0, 100, 50);
-const pointsList = arc.getPoints(20);
+const arc = new THREE.EllipseCurve(0, 0, 100, 50, 0, Math.PI / 2);
+const pointsList = arc.getPoints(50);
 
 const geometry = new THREE.BufferGeometry();
 geometry.setFromPoints(pointsList);
 
-const material = new THREE.PointsMaterial({
-  color: new THREE.Color('orange'),
-  size: 10
+
+const material = new THREE.LineBasicMaterial({
+  color: new THREE.Color('orange')
 });
 
-const points = new THREE.Points(geometry, material);
+const line = new THREE.Line(geometry, material);
 
-console.log(points);
 
-export default points;
+export default line;
