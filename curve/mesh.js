afdca9a65dcd746b72435149ba1ae8bb9e0818c5
imsunhao <imsunhao@vip.163.com>
2025-04-11 12:43:43 +0800

    feat: 学习 多条曲线复合

--- curve/mesh.js
+++ curve/mesh.js
@@ -1,30 +1,28 @@
 import * as THREE from 'three';
 
-const p1 = new THREE.Vector3(-100, 0, 0);
-const p2 = new THREE.Vector3(50, 100, 0);
-const p3 = new THREE.Vector3(100, 0, 100);
-const p4 = new THREE.Vector3(100, 0, 0);
+const p1 = new THREE.Vector2(0, 0);
+const p2 = new THREE.Vector2(100, 100);
+const line1 = new THREE.LineCurve(p1, p2);
 
-const curve = new THREE.CubicBezierCurve3(p1, p2, p3, p4);
-const pointsArr = curve.getPoints(20);
+const arc = new THREE.EllipseCurve(0, 100, 100, 100, 0, Math.PI);
 
+const p3 = new THREE.Vector2(-100, 100);
+const p4 = new THREE.Vector2(0, 0);
+const line2 = new THREE.LineCurve(p3, p4);
+
+const curvePath = new THREE.CurvePath();
+curvePath.add(line1);
+curvePath.add(arc);
+curvePath.add(line2);
+
+const pointsArr = curvePath.getPoints(20);
 const geometry = new THREE.BufferGeometry();
 geometry.setFromPoints(pointsArr);
 
 const material = new THREE.LineBasicMaterial({
-  color: new THREE.Color('orange')
+  color: new THREE.Color('pink')
 });
 
 const line = new THREE.Line(geometry, material);
-
-const geometry2 = new THREE.BufferGeometry();
-geometry2.setFromPoints([p1, p2, p3, p4]);
-const material2 = new THREE.PointsMaterial({
-  color: new THREE.Color('pink'),
-  size: 5
-});
-const points2 = new THREE.Points(geometry2, material2);
-const line2 = new THREE.Line(geometry2, new THREE.LineBasicMaterial());
-line.add(points2, line2);
 
 export default line;
