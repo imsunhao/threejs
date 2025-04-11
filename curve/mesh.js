251dc438aa3796468257a67cfc1b641f9a030c16
imsunhao <imsunhao@vip.163.com>
2025-04-11 12:42:02 +0800

    feat: 学习 三次贝塞尔曲线

--- curve/mesh.js
+++ curve/mesh.js
@@ -1,10 +1,11 @@
 import * as THREE from 'three';
 
-const p1 = new THREE.Vector2(0, 0);
-const p2 = new THREE.Vector2(50, 200);
-const p3 = new THREE.Vector2(100, 0);
+const p1 = new THREE.Vector3(-100, 0, 0);
+const p2 = new THREE.Vector3(50, 100, 0);
+const p3 = new THREE.Vector3(100, 0, 100);
+const p4 = new THREE.Vector3(100, 0, 0);
 
-const curve = new THREE.QuadraticBezierCurve(p1, p2, p3);
+const curve = new THREE.CubicBezierCurve3(p1, p2, p3, p4);
 const pointsArr = curve.getPoints(20);
 
 const geometry = new THREE.BufferGeometry();
@@ -17,7 +18,7 @@ const material = new THREE.LineBasicMaterial({
 const line = new THREE.Line(geometry, material);
 
 const geometry2 = new THREE.BufferGeometry();
-geometry2.setFromPoints([p1, p2, p3]);
+geometry2.setFromPoints([p1, p2, p3, p4]);
 const material2 = new THREE.PointsMaterial({
   color: new THREE.Color('pink'),
   size: 5
