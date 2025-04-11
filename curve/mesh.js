1397ec88301450ff2386b894d1efaf998b7ffb70
imsunhao <imsunhao@vip.163.com>
2025-04-11 12:38:49 +0800

    feat: 学习 SplineCurve

--- curve/mesh.js
+++ curve/mesh.js
@@ -1,17 +1,43 @@
 import * as THREE from 'three';
 
-const arc = new THREE.EllipseCurve(0, 0, 100, 50, 0, Math.PI / 2);
-const pointsList = arc.getPoints(50);
+const arr = [
+  new THREE.Vector2(-100, 0),
+  new THREE.Vector2(-50, 50),
+  new THREE.Vector2(0, 0),
+  new THREE.Vector2(50, -50),
+  new THREE.Vector2(100, 0)
+];
+
+const curve = new THREE.SplineCurve(arr);
+const pointsArr = curve.getPoints(50);
 
 const geometry = new THREE.BufferGeometry();
-geometry.setFromPoints(pointsList);
-
+geometry.setFromPoints(pointsArr);
 
 const material = new THREE.LineBasicMaterial({
   color: new THREE.Color('orange')
 });
 
 const line = new THREE.Line(geometry, material);
+
+
+const pointsMaterial = new THREE.PointsMaterial({
+  color: new THREE.Color('pink'),
+  size: 5
+});
+const points = new THREE.Points(geometry, pointsMaterial);
+line.add(points);
+
+
+const geometry2 = new THREE.BufferGeometry();
+geometry2.setFromPoints(arr);
+const material2 = new THREE.PointsMaterial({
+  color: new THREE.Color('green'),
+  size: 10
+});
+const points2 = new THREE.Points(geometry2, material2);
+const line2 = new THREE.Line(geometry2, new THREE.LineBasicMaterial());
+line.add(points2, line2);
 
 
 export default line;
