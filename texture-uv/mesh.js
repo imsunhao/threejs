b5bb264ff2195e0cb1d5df1023a2cf3fd92295d1
imsunhao <imsunhao@vip.163.com>
2025-04-11 12:13:03 +0800

    feat: 学习 uv 坐标

---
+++ texture-uv/mesh.js
@@ -0,0 +1,17 @@
+import * as THREE from 'three';
+
+const geometry = new THREE.PlaneGeometry(200, 100);
+
+const loader = new THREE.TextureLoader();
+const texture = loader.load('./bg.awebp');
+texture.colorSpace = THREE.SRGBColorSpace;
+
+const material = new THREE.MeshBasicMaterial(({
+  map: texture
+}));
+
+const mesh = new THREE.Mesh(geometry, material);
+
+console.log(mesh);
+
+export default mesh;
